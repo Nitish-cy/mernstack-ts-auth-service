123f103c9cc015825f8dfc48008b7fd53ab89f48
Nitish Mishra <Nitish Mishra>
2026-01-07 21:45:28 +0530

    prettier, husky and eslint added

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+  // 🔹 Ignore tooling & build files
+  {
+    ignores: [
+      'eslint.config.mjs',
+      'dist',
+      'node_modules',
+    ],
+  },
+
+  // 🔹 Base ESLint rules
+  eslint.configs.recommended,
+
+  // 🔹 TypeScript rules (type-checked)
+  tseslint.configs.recommendedTypeChecked,
+
+  // 🔹 TypeScript project service
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      'dot-notation':'error',
+    },
+  },
+);
---
+++ src/server.ts
@@ -0,0 +1,10 @@
+function welcome(name: string) {
+  const user = {
+    name: 'Nitish',
+  };
+  const u1 = user.name;
+  console.log('Name :', name);
+  return u1;
+}
+
+welcome('Mishra Nitish');
