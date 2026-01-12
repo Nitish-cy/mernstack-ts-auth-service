b9e4e6262e2f9a6332c1c38b1a6bb7fddc185f0b
Nitish Mishra <Nitish Mishra>
2026-01-12 20:09:45 +0530

    add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,48 @@
 import express from 'express';
+import type { Request, Response, NextFunction } from 'express';
+import logger from './config/logger.js';
+import createHttpError from 'http-errors';
+
 const app = express();
+
 app.get('/', (req, res) => {
-  res.send('Welcome to Outh Service');
+  res.send('Welcome to Auth Service');
 });
+
+// Global error handler
+app.use(
+  (
+    err: unknown,
+    req: Request,
+    res: Response,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    next: NextFunction,
+  ) => {
+    let statusCode = 500;
+    let message = 'Internal Server Error';
+
+    if (createHttpError.isHttpError(err)) {
+      statusCode = err.statusCode;
+      message = err.message;
+    } else if (err instanceof Error) {
+      message = err.message;
+    }
+
+    logger.error(message, {
+      path: req.path,
+      method: req.method,
+    });
+
+    res.status(statusCode).json({
+      errors: [
+        {
+          msg: message,
+          path: '',
+          location: '',
+        },
+      ],
+    });
+  },
+);
+
 export default app;
